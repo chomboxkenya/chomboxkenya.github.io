6e7a4645e7d73b85f72f2535d953ed98d78c2fb9
Alan Ruth <alan@notionlabs.com>
2015-06-07 21:14:51 -0600

    updating intro

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 
-
     /*---------------------------------------*/
     /*  STICKY NAVBAR
      /*---------------------------------------*/
@@ -22,7 +21,17 @@ $(document).ready(function() {
     stickySideMenu();
 
 
-
+    $("#js-rotating").Morphext({
+        // The [in] animation type. Refer to Animate.css for a list of available animations.
+        animation: "bounceIn",
+        // An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
+        separator: ",",
+        // The delay between the changing of each phrase in milliseconds.
+        speed: 2500,
+        complete: function () {
+            // Called after the entrance animation is executed.
+        }
+    });
 
     /* ======= Twitter Bootstrap hover dropdown ======= */
     /* Ref: https://github.com/CWSpear/bootstrap-hover-dropdown */ 
